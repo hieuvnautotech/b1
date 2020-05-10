996126a87657fd314e25d379eda36d84ae05e3d8
NN <hieuvnatutoech>
2020-05-10 11:47:48 +0700

    jashdkjasd

---
+++ WebApplication1/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DemoController : Controller
+    {
+        //local:port/demo/chuoi
+        public string Chuoi()
+        {
+            return "hello";
+        }
+        public int So() 
+        {
+            return new Random().Next(1000, 10000);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
